2a00a25aa9313dfcdb499742eb6ac543a264d8b9
Dante67yt <dante67yt@gmail.com>
2022-07-20 01:36:07 +0300

    add 2 vesion

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,5 +1,35 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
 
+// double[,] CreateMatr(int m, int n)
+// {
+//     double[,] matrix = new double[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().NextDouble()*100;
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatr(double[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(Math.Round(matrix[i, j],2) + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Console.Write("Введите количество строк в массиве: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов в массиве: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// PrintMatr(CreateMatr(m, n));
+
 Console.Write("Введите количество строк в массиве: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов в массиве: ");
