5dc8687afbc2729892fe88012baae2d7fcc05ddf
Dante67yt <dante67yt@gmail.com>
2022-07-19 21:15:47 +0300

    add task 1

---
+++ task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+double[,] CreateMatr(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble()*100;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatr(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(matrix[i, j],2) + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+PrintMatr(CreateMatr(m, n));
