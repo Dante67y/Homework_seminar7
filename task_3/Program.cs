1bd4fb9abd39f8cfb447d30ad222e56f5da8ae3e
Dante67yt <dante67yt@gmail.com>
2022-07-20 04:48:43 +0300

    add task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+
+int[,] CreateMatr(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+void AvgMatr(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        double avg = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            avg = avg + matrix[j, i];
+        }
+        Console.WriteLine(i + 1 + " столбец: " + avg/matrix.GetLength(0));
+    }
+}
+AvgMatr(CreateMatr(4, 4));
